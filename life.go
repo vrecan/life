2aa3558a04a66c43112e9768abd01a0e9a3f2a4f
michael.golowka <michael.golowka@logrhythm.com>
2019-03-21 16:33:04 -0600

    Use sync.Once instead of atomic.CompareAndSwapInt32

--- life.go
+++ life.go
@@ -2,35 +2,25 @@ package life
 
 import (
 	"sync"
-	"sync/atomic"
-)
-
-var (
-	open   int32 = 0
-	closed int32 = 1
 )
 
 // Life handles the creation of the background thread and shutdown management
 type Life struct {
-	wg     *sync.WaitGroup
-	Done   chan struct{}
-	run    func()
-	once   *sync.Once
-	closed *int32
+	wg    *sync.WaitGroup
+	Done  chan struct{}
+	run   func()
+	once  *sync.Once
+	close *sync.Once
 }
 
 // NewLife creates life with the expected defaults
 func NewLife() *Life {
 	return &Life{
-		wg:     &sync.WaitGroup{},
-		Done:   make(chan struct{}, 0),
-		once:   &sync.Once{},
-		closed: intptr(open),
+		wg:    &sync.WaitGroup{},
+		Done:  make(chan struct{}, 0),
+		once:  &sync.Once{},
+		close: &sync.Once{},
 	}
-}
-
-func intptr(i int32) *int32 {
-	return &i
 }
 
 // Start the background thread.
@@ -63,10 +53,9 @@ func (l Life) WGDone() {
 
 // Close will wait for the background thread to finish and then exit
 func (l Life) Close() error {
-	closeNow := atomic.CompareAndSwapInt32(l.closed, open, closed)
-	if closeNow {
+	l.close.Do(func() {
 		close(l.Done)
-	}
+	})
 	l.wg.Wait()
 	return nil
 }
